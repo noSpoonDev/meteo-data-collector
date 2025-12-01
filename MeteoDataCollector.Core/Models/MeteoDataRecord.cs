1e5b2e79b70924d6168dbe58591638f4ec4f13b9
lisnisam <samuel.lisnik@tietoevry.com>
2025-12-01 17:16:49 +0100

    feat(Core): define MeteoDataRecord model

---
+++ MeteoDataCollector.Core/Models/MeteoDataRecord.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeteoDataCollector.Core.Models;
+
+public class MeteoDataRecord
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    
+    public DateTime CollectedAt { get; set; } = DateTime.UtcNow;
+
+    public string? JsonData { get; set; }
+}
